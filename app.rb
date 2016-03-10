d1aebf32576a70a3a287ce6dc2cca195fca7ea52
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2016-03-10 11:44:35 +1100

    Added 'petrol' to reply words

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'twilio-ruby'
 post '/receive_sms' do
   content_type 'text/xml'
   message  = params[:Body].downcase
-  reply    = if message.scan(/refuel/).length > 0
+  reply    = if message.scan(/refuel/).length > 0 || message.scan(/petrol/).length > 0
     "Hello Josh, you vehicle MB890 will requires 77l costing $83. The refuel will take place at 30 Boronia St, 2016 Redfern in between 4pm to 5pm. Please second ‘YES’ to order the refuel."
   else
     "Hi Josh, your car has been refuelled. Have a good journey."
